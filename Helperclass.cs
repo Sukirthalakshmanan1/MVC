2b1f6210822de2f4b165e30a2579fc26e61be801
Sukirtha Lakshmanan <80877212+Sukirthalakshmanan1@users.noreply.github.com>
2022-10-09 23:10:46 +0530

    Files added

---
+++ Helperclass.cs
@@ -0,0 +1,51 @@
+﻿using BAL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class Helperclass
+    {
+        DAL1 dal = null;
+        public Helperclass()
+        {
+            dal = new DAL1();
+        }
+
+
+        public bool AddE(student school)
+        {
+            return dal.Insert(school);
+
+        }
+        //public bool AddEmployees(Employee_BAL employee)
+        //{
+        //    return dal.InsertEmployees(employee);
+
+        //}
+        public bool Edit(student school)
+        {
+            return dal.Update(school);
+        }
+        //public int Count()
+        //{
+        //    return dal.EmployeeCount();
+        //}
+        public student search(int id)
+        {
+            return dal.Find(id);
+        }
+        public List<student> List()
+        {
+            return dal.List();
+        }
+        public bool remove(int id)
+        {
+            return dal.Delete(id);
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class student
+    {
+        public int student_id { get; set; }
+        public string student_name { get; set; }
+
+        public int student_class { get; set; }
+    }
+    public class subjects
+    {
+        public int subjects_id { get; set; }
+        public string subjects_name { get; set; }    
+    }
+
+    public class Class1
+    {
+        public int Room_no { get; set; }
+        public int class_strength { get; set; }
+    }
+
+}
